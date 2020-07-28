5ef4ab20edac2a05d02fd3b0a60564fdddfdf99b
LiuZhaoyang <liuzhaoyanglzy620@163.com>
2020-07-28 17:33:55 +0800

    - change the password of mysql - change the theme of blog(using default now) - do some change of blog index.html

--- src/main/java/com/site/blog/my/core/controller/blog/MyBlogController.java
+++ src/main/java/com/site/blog/my/core/controller/blog/MyBlogController.java
@@ -26,7 +26,7 @@ public class MyBlogController {
 
     //public static String theme = "default";
     //public static String theme = "yummy-jekyll";
-    public static String theme = "amaze";
+    public static String theme = "default";
     @Resource
     private BlogService blogService;
     @Resource
